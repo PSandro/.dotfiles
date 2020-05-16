5fe7957014ac287e40b474457b86527b9caf59d1
Sandro Pischinger <sandro@psandro.eu>
2020-05-16 19:25:30 +0200

    :art: Improve zsh files fomat

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,24 +1,8 @@
 # Vars
-	HISTFILE=~/.zsh_history
-	SAVEHIST=1000
-	setopt inc_append_history # To save every command before it is executed
-	setopt share_history # setopt inc_append_history
-
-	git config --global push.default current
-
-# Settings
-
-#Functions
-
-# Custom cd
-
-# For vim mappings:
-
-# Completions
-# These are all the plugin options available: https://github.com/robbyrussell/oh-my-zsh/tree/291e96dcd034750fbe7473482508c08833b168e3/plugins
-#
-# Edit the array below, or relocate it to ~/.zshrc before anything is sourced
-# For help create an issue at github.com/parth/dotfiles
+HISTFILE=~/.zsh_history
+SAVEHIST=1000
+setopt inc_append_history # To save every command before it is executed
+setopt share_history # setopt inc_append_history
 
 autoload -U compinit
 
--- zsh/zshrcd.zsh
+++ zsh/zshrcd.zsh
@@ -1,18 +1,2 @@
 export DOTFILES_HOME="$HOME/.dotfiles" #TODO: make dynamic dir name accessable
-
-function dotfiles-update() {
-  echo "Checking for updates."
-  ({cd $DOTFILES_HOME && git fetch -q} &> /dev/null)
-
-  if [ $({cd ~/.dotfiles} &> /dev/null && git rev-list HEAD...origin/master | wc -l) = 0 ]
-  then
-  	echo "Already up to date."
-  else
-  	echo "Updates Detected:"
-  	({cd $DOTFILES_HOME} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd)
-  	echo "Setting up..."
-  	({cd $DOTFILES_HOME} &> /dev/null && git pull -q && git submodule update --init --recursive)
-  fi
-}
-
 source $DOTFILES_HOME/zsh/zshrc.zsh
