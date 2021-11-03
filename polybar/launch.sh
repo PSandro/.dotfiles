912eb77ed7a60c119063f3a0ff99fab2c72cbcb7
Sandro Pischinger <mail@sandropischinger.de>
2021-11-03 23:00:12 +0100

    fix config path

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.dotfiles/polybar/config main 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar laptop-bar 2>&1 | tee -a /tmp/polybar2.log & disown
 echo "Bars launched..."
