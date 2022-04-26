144a0f8b4ad0562c70c1bc2945979cba497c9ef7
Sandro Pischinger <mail@sandropischinger.de>
2022-04-27 01:04:24 +0200

    reformat zshrc.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,6 +5,10 @@ DOTFILES_HOME="$HOME/.dotfiles"
 setopt inc_append_history # To save every command before it is executed
 setopt share_history # setopt inc_append_history
 
+source $DOTFILES_HOME/zsh/window.zsh
+source $DOTFILES_HOME/zsh/config.zsh
+source $DOTFILES_HOME/zsh/prompt.zsh
+
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
@@ -25,18 +29,24 @@ zinit light-mode for \
     zdharma-continuum/zinit-annex-rust
 
 ### End of Zinit's installer chunk
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
 
 autoload -U compinit
+zmodload zsh/complist
+compinit
 
-source $DOTFILES_HOME/zsh/window.zsh
-source $DOTFILES_HOME/zsh/completion.zsh
-source $DOTFILES_HOME/zsh/config.zsh
-source $DOTFILES_HOME/zsh/prompt.zsh
+zinit wait lucid light-mode for \
+  atinit"zicompinit; zicdreplay" \
+      zdharma-continuum/fast-syntax-highlighting \
+  atload"_zsh_autosuggest_start" \
+      zsh-users/zsh-autosuggestions
+
 
 
 export PATH=$PATH:$DOTFILES_HOME/util
 export PATH=$PATH:$HOME/.local/bin
 
-zinit for \
-    light-mode  zsh-users/zsh-autosuggestions \
-    light-mode  zdharma-continuum/fast-syntax-highlighting \
