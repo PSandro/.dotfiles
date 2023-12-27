9e3810959c97e7c0114d87dcfe8f5338df6ec68c
Sandro Pischinger <mail@sandropischinger.de>
2023-12-28 00:58:12 +0100

    merge zshrc stuff

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,6 +9,90 @@ source $DOTFILES_HOME/zsh/window.zsh
 source $DOTFILES_HOME/zsh/config.zsh
 source $DOTFILES_HOME/zsh/prompt.zsh
 
+### WINDOW
+function title() {
+  # escape '%' chars in $1, make nonprintables visible
+  a=${(V)1//\%/\%\%}
+  # Truncate command, and join lines.
+  a=$(print -Pn "%40>...>$a" | tr -d "\n")
+  case $TERM in
+  screen)
+    print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
+    ;;
+  xterm*|rxvt)
+    print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
+    ;;
+  esac
+}
+
+### CONFIG
+export LSCOLORS="exfxcxdxbxegedabagacad"
+export CLICOLOR=true
+
+# Colorize when possible
+alias ls="exa"
+alias grep="grep --color=auto"
+alias diff="diff --color=auto"
+alias ip="ip -c"
+alias vim="nvim"
+# alias cat="bat"
+
+# Set FSH THEME_NAME
+#export FAST_THEME_NAME="clean"
+
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+# Use vim keys in tab complete menu:
+#bindkey -M menuselect 'j' vi-backward-char
+#bindkey -M menuselect 'l' vi-up-line-or-history
+#bindkey -M menuselect 'ö' vi-forward-char
+#bindkey -M menuselect 'k' vi-down-line-or-history
+#bindkey -v '^?' backward-delete-char
+
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
+
+function man() {
+	env \
+		LESS_TERMCAP_md=$(tput bold; tput setaf 4) \
+		LESS_TERMCAP_me=$(tput sgr0) \
+		LESS_TERMCAP_mb=$(tput blink) \
+		LESS_TERMCAP_us=$(tput setaf 2) \
+		LESS_TERMCAP_ue=$(tput sgr0) \
+		LESS_TERMCAP_so=$(tput smso) \
+		LESS_TERMCAP_se=$(tput rmso) \
+		PAGER="${commands[less]:-$PAGER}" \
+		man "$@"
+}
+
+### PROMPT
+autoload -U colors && colors
+
+BLACK=59;
+RED=166;
+GREEN=72;
+YELLOW=186;
+BLUE=24;
+PURPLE=168;
+LIGHT_BLUE=38;
+GREY=230;
+
+set_prompt () {
+  PS1="%B%{%F{$RED}%}[%{%F{$YELLOW}%}%n%{%F{$GREEN}%}@%{%F{$BLUE}%}%M %{%F{$PURPLE}%}%~%{%F{$RED}%}]%{$reset_color%}$%b "
+}
+
+
+git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+precmd () {
+  title "zsh" "%m" "%55<...<%~"
+  set_prompt
+}
+
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
