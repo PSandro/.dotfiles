c3be571b040878b52fb78ca60f28d86c04721906
Sandro Pischinger <mail@sandropischinger.de>
2023-12-28 20:22:53 +0100

    fix polybar again

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar --config=$HOME/.dotfiles/polybar/config main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=$HOME/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar laptop-bar 2>&1 | tee -a /tmp/polybar2.log & disown
 echo "Bars launched..."
